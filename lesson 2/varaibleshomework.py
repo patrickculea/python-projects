33b96ed365674594f928e5103a2242336e5cc5de
patrickculea <patrickculea1@gmail.com>
2026-02-24 20:08:17 +0000

    variableshomework done

---
+++ lesson 2/varaibleshomework.py
@@ -0,0 +1,7 @@
+mybirthday = ("30 of march 2013")
+dadsbirthday = ("31 of july 1978")
+mumbirthday = ("30 of january 1973")
+maxbirthday = ("30 of march 2013")
+davidbirthday = ("19 of december 2014")
+
+print("\nhello i was born on the", mybirthday,"\nmy dads was born on the",dadsbirthday,"\nmy mum was born on the",mumbirthday,"\nmax was born on the",maxbirthday,"\nand finally , david was born on the",davidbirthday)
