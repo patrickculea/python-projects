baf7cb967aa601599ff843dc1798b338b71e9970
patrickculea <patrickculea1@gmail.com>
2026-03-10 19:49:25 +0000

    lesson 4 done

---
+++ lesson 4/activity1.py
@@ -0,0 +1,9 @@
+t1 =98
+t2 =94
+t3 =41
+t4 =96
+t5 =11
+
+total =t1 +t2 +t3 +t4 +t5
+avg =total / 5
+print("average height =",avg)
---
+++ lesson 4/activity2.py
@@ -0,0 +1,12 @@
+amount =int(input("input an amount="))
+note_500 =amount // 500
+print("note_500=",note_500)
+amount %= 500
+note_100 = amount // 100
+# * is multiplying
+# % is the remainder
+# // is the integer of the division
+# / is normal division but gives you the result in float
+print("note_100=",note_100)
+
+
