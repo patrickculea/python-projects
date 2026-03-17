8eabfba4717e290fc1a67bdc6e110b30292371b8
patrickculea <patrickculea1@gmail.com>
2026-03-17 20:07:56 +0000

    done temphomework.py

---
+++ lesson 5/temphomework.py
@@ -0,0 +1,5 @@
+temp =int(input("what is the temperature outside?,in degrees celcius"))
+if temp > 12:
+    print("its warm enough to wear lighter clothes")
+else:
+    print("its too cold, you should keep wearing the jacket")
