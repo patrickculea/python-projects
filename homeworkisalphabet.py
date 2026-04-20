2c7428f6e34a8702befbeed1fcd946726174328c
patrickculea <patrickculea1@gmail.com>
2026-04-20 18:41:50 +0100

    done homeworkisalphabet.py

---
+++ homeworkisalphabet.py
@@ -0,0 +1,5 @@
+alpha =input("give me a string and I will tell you if its a letter of the alphabet or not.")
+if alpha.isalpha() :
+    print ("the string you have given me is made of letters")
+else :
+    print("the string you have given me is not made up of only letters")
