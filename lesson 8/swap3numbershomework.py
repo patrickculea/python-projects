cf5308f2b74429285e25638f304d68e0b3e456f2
patrickculea <patrickculea1@gmail.com>
2026-05-05 21:35:01 +0100

    done swap3numbershomwork.py

---
+++ lesson 8/swap3numbershomework.py
@@ -0,0 +1,6 @@
+a =int(input("what should the value of a be?, must be whole value"))
+b =int(input("what should the value of b be?, must be a whole number"))
+c =int(input("what should the value of c be?, must be whole number"))
+print(f"the values you have just given me are a={a}, b ={b}, and c ={c}")
+a ,b ,c = c, a, b
+print(f"the swapped values are: a={a}, b={b}, and c={c}")
