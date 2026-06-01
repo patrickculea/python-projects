9d95c549db52bf55c617e063d84febc27d0c0919
patrickculea <patrickculea1@gmail.com>
2026-06-01 17:57:22 +0100

    done agecheckhomework.py

---
+++ lesson 2/lesson 9/agecheckhomework.py
@@ -0,0 +1,12 @@
+age = int(input("how old are you ?"))
+if age < 10 :
+    print(f"you are younger than ten, you are {age}.")
+
+if age == 10:
+    print("you are ten.")
+
+if (age > 10 and age < 20):
+    print(f"You are older than 10 but younger than 20, you are {age}")
+
+if age > 20:
+    print(f"you are older than 20, you are {age}.")
