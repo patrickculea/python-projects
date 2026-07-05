33abada73ce01032c558683943fd21dd610e4202
patrickculea <patrickculea1@gmail.com>
2026-07-05 19:52:40 +0100

    done turtlesquarehomework

---
+++ assessment1/project.py
@@ -0,0 +1,13 @@
+iny = int(input("Guess what my secret number is."))
+secret = 27
+while g <= 5:
+    if iny == 27:
+        print("Well done, you guessed my random number.")
+else:
+    print("You guessed wrong.")
+    if iny <= 22  and iny >= 32:
+    print("you are hot")
+
+
+
+
---
+++ lesson 13/turtlesquarehomework.py
@@ -0,0 +1,8 @@
+l = int(input("How long should the square be?"))
+import turtle
+t = turtle.Turtle()
+
+for i in range(4):
+    t.forward(l)
+    t.left(90)
+
